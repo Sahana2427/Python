e2ba3ed26eddec63dc4af8b2f5d53ed07910ecf1
Sahana2427 <sahanasana242@gmail.com>
2024-10-12 17:00:16 +0530

    Built In Function

---
+++ Built_In_Function.py
@@ -0,0 +1,21 @@
+# id memory address
+"""x = 5
+print(id(x))"""
+
+# absolute
+"""x = 9.9
+print(abs(x))"""
+
+#str conversion
+"""y = 2
+z = str(y)
+print(z)
+
+print(type(z))"""
+
+# enumerate  -- to assign id's 
+
+"""languages = ["python","c","java"]
+enumerate_prime = enumerate(languages,2)
+print(list(enumerate_prime))"""
+
