22305b2d71b99b41de4c8bed1bb0070a04b673b5
Sahana2427 <sahanasana242@gmail.com>
2024-10-12 17:03:33 +0530

    CF revison

---
+++ CF_Revison.py
@@ -0,0 +1,36 @@
+"""user_name = "sahana"
+password = "magic@1234"
+
+if user_name == input("Enter username") and password == input("Enter password"):
+    print("Logged IN ")
+
+else:
+    print("Incorrect password and username")"""
+                     
+""""mobile_no = 8796453632
+otp = 1234
+
+if mobile_no == int(input("enter mobile number")):
+    print("Yes entered correct mobile no")
+    if otp == int(input("enter otp")):
+        print("correct otp")
+    else:
+        print("incorrect otp")
+else:
+    print("incorrect mobile no")"""
+
+"""l = [1,2,3,4,56,"sudh","sahana"]
+l_str = []
+l_int = []
+for i in l:
+    if type(i) == str:
+        l_str.append(i)
+    if type(i) == int:
+        l_int.append(i)
+print(l_str)
+
+
+
+print(l_int)"""
+
+
