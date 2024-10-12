14d17bc5c1f8894b119d4924c10d1bd14f54cb55
Sahana2427 <sahanasana242@gmail.com>
2024-10-12 17:13:03 +0530

    Logical Opearator

---
+++ Logical_Operators.py
@@ -0,0 +1,81 @@
+a = 10
+b = 20
+
+# AND #
+print("                                        AND                                                ")
+
+print((a > 9) and (b > 19))     # Both coditions satisfies "True" #
+
+print((a >= 10) and (b > 20))   # One condition satisfies "False" #
+
+print((a > 10) and (b > 20))    # Both conditions does not satisfy "False" #
+
+# OR #
+
+print("                                        OR                                                ")
+
+
+print((a > 9) or (b > 19))     # Both condition satisfies "True" #
+
+print((a >= 10) or (b > 20))   # One condition satisfies "True" #
+
+print((a > 10) or (b > 20))    # Both conditions does not satisfy "False" #
+
+print("                                    TRUE OR FALSE                                 ")
+print(True and False)
+print(True and True)
+print(True or False)
+print(True or True)
+print(False or False)
+
+# NOT #
+
+print("                                    NOT                                ")
+c = True
+print(not(c))
+
+print("                                    BITWISE OPERATOR                                 ")
+
+''' & -- AND
+    | ---OR
+    ~ --- NEGATION 
+    ^ --- XOR
+    << -- LEFT SHIFT
+    >> -- RIGHT SHIFT'''
+
+# BINARY #
+
+print("                                    DECIMAL TO BINARY                                 ")
+
+
+d = 10
+print("THE BINARY VALUE OF 10 IS :" ,bin(d))
+
+print(a&b)
+print(a|b)
+print(a^b)
+print(~a)  # Take 2's complement #
+e = 8  # 8 + 1 = -9 #
+f = -15
+print(~f)
+
+
+print(~e)
+
+print((~-12))
+
+print("----------------------------------------------------------------------------------------")
+x = [1,2,3,4]
+
+print(2 in x)  # IN operator #
+
+print(a<<1)
+print(a>>1)57
+
+
+
+print(a^b)
+
+
+
+
