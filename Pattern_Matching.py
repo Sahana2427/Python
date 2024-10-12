cf7f8e1598a20a951ec7d7aeee559381c48f6895
Sahana2427 <sahanasana242@gmail.com>
2024-10-12 17:14:18 +0530

    Pattern Matching

---
+++ Pattern_Matching.py
@@ -0,0 +1,17 @@
+"""for i in range(5):
+    for j in range(i+1):
+        print("*", end= " ")
+    print()
+
+
+for i in range(5,0,-1):
+    for j in range(i,0,-1):
+        print("*", end= " ")
+    print()"""
+
+
+for i in range(1,6):
+    for j in range(1,6):
+        print(i*j,end= " ")
+    print()
+
