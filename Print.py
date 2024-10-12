80ae0ee68f5f48a435eee11530c14ae654f7737c
Sahana2427 <sahanasana242@gmail.com>
2024-10-12 17:14:39 +0530

    Print

---
+++ Print.py
@@ -0,0 +1,15 @@
+x = "snap"
+y = "I use"
+
+print("hello {name}, i am ur {friend}".format(name = "sahana", friend = "vibha"))
+
+
+print("Hii")
+print("Sahana")
+
+print("Hii", end = ' ')
+print("Sahana", end = ' ')
+
+print("Hii", end = '@ ')
+print("Sahana", end = ' @')
+
