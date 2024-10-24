6e112311ee0307ea1ae7a97b6693ea9e33b98942
Sahana2427 <sahanasana242@gmail.com>
2024-10-24 15:50:08 +0530

    OOPS_Calculator

---
+++ OOPS_4_Calculator.py
@@ -0,0 +1,15 @@
+from functools import reduce 
+
+class calculator:
+
+    def add(self,*args):
+        return sum(args)
+    
+    def subtraction(self,*args):
+        return reduce(lambda a,b : a-b,args)
+    
+himanshu = calculator()
+
+print(himanshu.add(1,2,3,4))
+    
+
